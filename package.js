579d46b1ccd2ff0a7b8d072fdce305961c96127b
Jan Kuester <jkuester@uni-bremen.de>
2019-11-15 10:41:49 +0100

    published 1.0.4

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'leaonline:ddp-login',
-  version: '1.0.3',
+  version: '1.0.4',
   // Brief, one-line summary of the package.
   summary: 'Provide loginWithLea to a DDP connection.',
   // URL to the Git repository containing the source code for this package.
